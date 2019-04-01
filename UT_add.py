4ff5e9135879835acdd6a8f69ba3f4c23074dcbf
ibananas <48821196+ibananas@users.noreply.github.com>
2019-04-01 23:11:40 +0800

    Update UT_add.py

--- UT_add.py
+++ UT_add.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-# @Time     : 2018/09/01 00:00
+# @Time     : 2019/04/01 17:00
 # @Author   : 刘洪波
 # @Filename : 默认.py
 # @Software : PyCharm
 
 import unittest
 from python_add_number import num_add
+from python_add_number import tool
 from mock import Mock,patch
 
 class TestProducer(unittest.TestCase):
@@ -16,8 +17,9 @@ class TestProducer(unittest.TestCase):
         self.num_add = num_add()
         mock_add1.return_value = 1
         mock_add2.return_value = 2
-        num1 = "2649821731631836529481632803462831616487712734074314936141303241873417434716340124362304724324324324324323412121323164329751831"
-        num2 = "1045091731748365195814509145981509438583247509149821493213241431431319999999999999999999999999999999999999999999999999341344779"
+        self.u = tool()
+        num1 = self.u.random_number(100000)
+        num2 = self.u.random_number(2500000)
         self.assertEqual(self.num_add.add1(num1, num1), 1)
         self.assertEqual(self.num_add.add2(num1, num2), 2)
 
